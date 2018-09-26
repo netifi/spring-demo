0abc9c1babb3f8de5c06ea022ebb9587fc87fdd8
Stephane Maldini <smaldini@pivotal.io>
2018-09-25 21:09:02 -0700

    cache count

--- records/src/main/java/io/rsocket/springone/demo/DefaultRecordsService.java
+++ records/src/main/java/io/rsocket/springone/demo/DefaultRecordsService.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import io.netty.buffer.ByteBuf;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,17 +16,21 @@ public class DefaultRecordsService implements RecordsService {
 
   private final Random r = new Random();
 
+  private final Mono<Integer> total;
+
   public DefaultRecordsService(@Autowired RecordRepository recordRepository) {
       this.recordRepository = recordRepository;
+	  this.total = recordRepository.count()
+	                               .map(Long::intValue)
+	                               .cache();
   }
 
   @Override
   public Flux<Record> records(RecordsRequest request, ByteBuf metadata) {
-    return recordRepository.count()
-                    .map(Long::intValue)
-                    .flatMapMany(c ->
-                            this.recordRepository.findAllByThumbnailNotNull(offset(request, c), request.getMaxResults())
-                                                 .map(DbRecord::toRecord));
+    return total.log()
+                .flatMapMany(c ->
+		                this.recordRepository.findAllByThumbnailNotNull(offset(request, c), request.getMaxResults())
+		                                     .map(DbRecord::toRecord));
 //    return this.recordRepository
 //            .findAllByThumbnailNotNull(request.getOffset(), request.getMaxResults())
 //            .map(DbRecord::toRecord);
